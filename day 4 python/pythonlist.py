9259395d4f964497b5aced07cc0d5624b75f1f83
Rohit ashva <rohzxz@gmail.com>
2023-08-02 17:35:38 +0530

    days 4 adding
    rock paper...games

--- day 4 python/pythonlist.py
+++ day 4 python/pythonlist.py
@@ -34,6 +34,13 @@ position = input("Where do you want to put the treasure? ")
 
 # Write your code below this row 👇
 
+horizon = int(position[0])
+vertical = int(position[1])
+
+selected_row =(map[vertical - 1])
+selected_row[horizon - 1] = "X"
+
+
 
 # Write your code above this row 👆
 
